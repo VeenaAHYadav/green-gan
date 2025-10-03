610aa6d124786cdaa7c43cf1bebf9ab67b6c07c7
Veena Yadav <veenayadav7975@gmail.com>
2025-10-03 20:49:01 +0530

    Update download_data.sh

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -1,1 +1,8 @@
 
+#!/bin/bash
+# Download CICIDS2017 (public)
+# Instructions/manual, as direct bulk download may require registration for http://www.unb.ca/cic/datasets/ids-2017.html
+
+echo "Please visit https://www.unb.ca/cic/datasets/ids-2017.html, request access, and download 'MachineLearningCSV.zip'."
+echo "Unzip all CSV files into data/raw/."
+echo "You should see files like Friday-WorkingHours-Afternoon-DDos.pcap_ISCX.csv, etc."
