1d029afe29dacb19bd0c5bc0a1f76b2d77e932ea
Veena Yadav <veenayadav7975@gmail.com>
2025-10-03 20:55:10 +0530

    Update adversarial_attacks.py

--- scripts/adversarial_attacks.py
+++ scripts/adversarial_attacks.py
@@ -1,1 +1,6 @@
-
+def fgsm_attack(x, y, model, epsilon, loss_fn):
+    x_adv = x.clone().detach().requires_grad_(True)
+    output = model(x_adv)
+    loss = loss_fn(output, y)
+    loss.backward()
+    return x_adv + epsilon*x_adv.grad.sign()
